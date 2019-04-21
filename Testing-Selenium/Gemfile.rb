3f3ac7976d9ba09ec97a10117ae7515a739ceafd
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-21 03:12:36 +0200

    Add files via upload

---
+++ Testing-Selenium/Gemfile.rb
@@ -0,0 +1,10 @@
+gem install cucumber
+gem install capybara
+gem install selenium-webdriver
+gem install rspec
+
+
+
+gem install poltergeist
+gem install capybara poltergeist
+gem install phantomjs
